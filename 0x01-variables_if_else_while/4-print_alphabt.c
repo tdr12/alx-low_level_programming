5d6f9bee99de9e64c88e1ecb8e0cf9935222e284
tdr12 <delanodray@gmail.com>
2022-10-23 19:19:21 -0700

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		if (lowercase != 'e' && lowercase != 'q')
+			continue;
+		putchar(lowercase);
+	}
+	putchar('\n');
+	return (0);
+}
