aacfa4701f5e7fc113170e8df332a545596f93e7
tdr12 <delanodray@gmail.com>
2022-10-23 18:59:29 -0700

    Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,8 @@ int main(void)
 
 	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 		putchar(lowercase);
-
 	for (lowercase = 'A'; lowercase <= 'Z'; lowercase++)
 		putchar(lowercase);
-		putchar('\n');
-
+	putchar('\n');
 	return (0);
 }
